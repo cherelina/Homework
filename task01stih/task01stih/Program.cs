aa5e0b7fb1058e06067092e8ced4836852002b6d
Ангелина <angelinacherepkova@yandex.ru>
2024-09-23 17:04:22 +0500

    the first task is done

---
+++ task01stih/task01stih/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01stih
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Зимний вечер");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Буря мглою небо кроет,");
+            Console.WriteLine("Вихри снежные крутя;");
+            Console.WriteLine("То как зверь она завоет,");
+            Console.WriteLine("То заплачет как дитя...");
+
+            Console.ResetColor();
+
+            Console.ReadKey ();
+        }
+    }
+}
