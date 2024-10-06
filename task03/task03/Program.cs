c1ed2ac2aa680d61aa2e7531383a398621bd1e57
Ангелина <angelinacherepkova@yandex.ru>
2024-10-06 17:22:54 +0500

    Задание 3 выполнено

---
+++ task03/task03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырехзначное число");
+            var a = int.Parse(Console.ReadLine());
+
+            var thousands = a / 1000;
+            var hundreds = (a / 100) % 10;
+            var tens = (a / 10) % 10;
+            var units = a % 10;
+
+            var result = thousands * 1000 + tens * 100 + units * 10 + hundreds;
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
