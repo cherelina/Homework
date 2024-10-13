6c379ae3776d5b4b414031a7ac9f1c1e9c742ea4
Ангелина <angelinacherepkova@yandex.ru>
2024-10-13 15:18:50 +0500

    Задание 5 выполнено

---
+++ task05/task05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = F(2, 3) + F(3, 5) + F(4, 3);
+            Console.WriteLine($"x= {x:F3}");
+
+            Console.ReadKey();
+
+        }
+        static double F(double a, double b)
+        {
+            return (Math.Sin((Math.Pow(a, a) + 1) / (Math.Pow(b, b) + 1)));
+        }
+    }
+}
