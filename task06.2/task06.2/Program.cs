ae551bd804563d73d50f780087956c11c5031ca5
Ангелина <angelinacherepkova@yandex.ru>
2024-10-20 20:54:41 +0500

    Задание 6.2 выполнено

--- task06.2/task06.2/Program.cs
+++ task06.2/task06.2/Program.cs
@@ -10,7 +10,33 @@ namespace task06._2
     {
         static void Main(string[] args)
         {
+            var s = "прогулка";
+            Console.WriteLine($"Из слова \"{s}\"получили");
+
+            var word1 = s.Remove(4, 2)
+                .Remove(0, 1)
+                .Substring(2, 1) +
+                s.Substring(2, 1) +
+                s.Substring(1, 1) +
+                s.Substring(6, 2);
+
+            Console.WriteLine(word1);
+
+            var word2 = s.Remove(4, 4)
+                .Substring(0, 1) +
+                s.Substring(2, 1) +
+                s.Substring(1, 2) +
+                s.Substring(3, 1);
+
+                
+
+            Console.WriteLine(word2);
+
+            Console.ReadKey();
 
         }
+       
+        
     }
+
 }
