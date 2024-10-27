9851d1877aed50e28acfb8788fc0f91106bb0417
Ангелина <angelinacherepkova@yandex.ru>
2024-10-28 00:43:31 +0500

    Задание 7.1 выполнено

---
+++ task7.1/task7.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = GetNumber("n");
+
+            if (IsStatementTrue(n))
+                Console.WriteLine("Утверждение истино");
+            else
+                Console.WriteLine("Утверждение ложно");
+
+            Console.ReadKey();
+        }
+
+        static bool IsStatementTrue(int n)
+        {
+            return n % 5 != 0 && n % 8 != 0;
+        }
+
+        static int GetNumber(string numberName)
+        {
+            Console.WriteLine($"Введите число {numberName}");
+            return int.Parse(Console.ReadLine());
+        }
+
+    }
+}
+
