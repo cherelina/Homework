47b8e2063657f64ccf20805bcd26de31335e9ea3
Ангелина <angelinacherepkova@yandex.ru>
2024-10-28 00:29:04 +0500

    Задание 7.2 выполнено

---
+++ task07.3/task07.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите позицию белой фигуры");
+            var whiteElephantPosition = Console.ReadLine();
+
+            var whiteElephantRow, whiteElephantColumn;
+
+            
+            Console.WriteLine();
+        }
+        static void DecodePosition(string position, out int x, out int y)
+        {
+            x = int.Parse(position[1].ToString());
+            y = (int)position[0] - 0x60;
+        }
+    }
+}
