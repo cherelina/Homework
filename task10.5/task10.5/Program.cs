d9d0a428803e3641f9079d0c3ef7e1dba8bbaa00
Ангелина <angelinacherepkova@yandex.ru>
2024-12-09 01:41:53 +0500

    Задание 10.5 выполнено

---
+++ task10.5/task10.5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число");
+            int number;
+            if (!int.TryParse(Console.ReadLine(), out number) || number < 1)
+            {
+                Console.WriteLine("Ошибка ввода");
+                Console.ReadKey();
+                return;
+            }
+
+            int largestPrimeDivisor = 1; 
+            int temp = number;
+
+            
+            for (int i = 2; i <= Math.Sqrt(temp); i++)
+            {
+                
+                while (temp % i == 0)
+                {
+                    largestPrimeDivisor = i; 
+                    temp /= i; 
+                }
+            }
+
+            
+            if (temp > 1)
+            {
+                largestPrimeDivisor = temp;
+            }
+
+            Console.WriteLine($"Наибольший простой делитель числа {number} равен {largestPrimeDivisor}");
+            Console.ReadKey();
+        }
+    }
+}
+    
