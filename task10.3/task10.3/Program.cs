a3f2aadcc268d28f1c112bbd727a0333f43fd86c
Ангелина <angelinacherepkova@yandex.ru>
2024-12-09 01:31:19 +0500

    Задание 10.3 сделано

---
+++ task10.3/task10.3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double a;
+                do
+                {
+                    Console.Write("Введите число a (a > 1): ");
+                } while (!double.TryParse(Console.ReadLine(), out a) || a <= 1);
+
+                double sum = 0;
+                int n = 0;
+
+                Console.WriteLine($"Сумма будет вычисляться до тех пор, пока она меньше {a}:");
+
+                while (sum < a)
+                {
+                    n++;
+                    sum += 1.0 / n;
+                    if (sum < a)
+                    {
+                        Console.WriteLine($"n = {n}, сумма = {sum}");
+                    }
+                }
+
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+
+    }
+
