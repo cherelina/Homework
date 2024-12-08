65d69c3ad464217757ea1c4708358761251f416d
Ангелина <angelinacherepkova@yandex.ru>
2024-12-09 01:20:20 +0500

    Задание 10.2 выполнено

---
+++ task10.2/task10.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.Write("Введите натуральное число n: ");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n < 2)
+                {
+                    Console.WriteLine("Для вычисления произведения должно быть как минимум два числа");
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.Write("Введите a0: ");
+                double previous = double.Parse(Console.ReadLine());
+
+                double current;
+                double lastProduct;
+                string products = "";
+                int i = 1;
+
+                while (i <= n)
+                {
+                    Console.Write($"Введите a{i}: ");
+                    current = double.Parse(Console.ReadLine());
+
+                    lastProduct = previous * current;
+                    products += lastProduct + ", ";
+
+                    previous = current;
+                    i++;
+                }
+
+                Console.WriteLine($"Последовательность произведений соседних чисел: {products}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+    
+
---
+++ task10.2/task10.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task10.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("task10.2")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f528b885-70de-4286-8b51-af309420b52c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
